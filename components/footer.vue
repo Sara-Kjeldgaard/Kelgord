e498e57754607b5d5fb2732e5fd3ea4460ea85af
Sara S Kjeldgaard <sara.kjeldgaard@gmail.com>
2020-12-11 17:48:48 +0100

    Landing and about

--- components/footer.vue
+++ components/footer.vue
@@ -1,19 +1,46 @@
 <template>
     <footer>
-        <div id="mailFooter">
+        <a class="contact-footer">
             <img src="/images/Mail.svg" />
-            <a>kontakt@kelgord.dk </a>
-        </div>
-        <div id="phoneFooter">
-           <img src="/images/Phone.svg" />
-            <a>+45 12343456</a>
-        </div>
-        <div id="footerSoMe">
+            <span>kontakt@kelgord.dk </span>
+        </a>
+        <a class="contact-footer">
+            <img src="/images/Phone.svg" />
+            <span>+45 12343456</span>
+        </a>
+        <span>Savonlinnavej 20, 8600 Silkeborg, DK</span>
+        <a id="soMeFooter">
             <img src="/images/Linkedin.svg" />
-        </div>
+        </a>
     </footer>
 </template>
 
 <style>
+footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0;
+}
 
+.contact-footer {
+    border: 1px solid #d66b00;
+    border-radius: 30px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 5px 20px;
+    margin-right: 30px;
+}
+
+.contact-footer img {
+    height: 80%;
+    padding-right: 20px;
+}
+
+#soMeFooter img {
+    width: 70px;
+    margin-left: 300px;
+}
 </style>
--- components/header.vue
+++ components/header.vue
@@ -6,19 +6,29 @@
             </a>
             <ul class="nav-list">
                 <li>
-                    <a v-bind:class="{active: active === 'OmKelgord'}"  href="">Om Kelgord</a>
+                    <a v-bind:class="{ active: active === 'OmKelgord' }" href=""
+                        >Om Kelgord</a
+                    >
                 </li>
                 <li>
-                    <a class="active" href="">Kerneværdier</a>
+                    <a v-bind:class="{ active: active === 'Kerneværdier' }" href=""
+                        >Kerneværdier</a
+                    >
                 </li>
                 <li>
-                    <a href="">Kompetencer</a>
+                    <a v-bind:class="{ active: active === 'Kompetencer' }" href=""
+                        >Kompetencer</a
+                    >
                 </li>
                 <li>
-                    <a href="">Konsulentydelser</a>
+                    <a v-bind:class="{ active: active === 'Konsulentydelser' }" href=""
+                        >Konsulentydelser</a
+                    >
                 </li>
                 <li>
-                    <a href="">Kontakt</a>
+                    <a v-bind:class="{ active: active === 'Kontakt' }" href=""
+                        >Kontakt</a
+                    >
                 </li>
             </ul>
         </nav>
@@ -26,9 +36,14 @@
 </template>
 
 <script>
-module.export = {
+/* (module.exports) dette er js som fortæller Vue at det vi sætter det lig med er js delen af vores komponent. 
+    Kildehenvisning: https://vuejs.org/v2/guide/single-file-components.html*/
+
+module.exports = {
+    /*(props) Bruges til at give retten til at angive en variabel til en komponent i Vue
+    (active) Er en variabel til en komponent i Vue*/
     props: ["active"]
-}
+};
 </script>
 
 <style>
@@ -59,10 +74,11 @@ nav {
     width: 180px;
     padding: 0 20x;
     text-align: center;
-    transition: all .2s ease; 
+    transition: all 0.2s ease;
 }
 
-.nav-list a:hover,  .nav-list .active {
+.nav-list a:hover,
+.nav-list .active {
     color: #d66b00;
     font-weight: bold;
 }
