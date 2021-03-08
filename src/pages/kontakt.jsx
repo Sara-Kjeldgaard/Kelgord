f0f916b48af218eafbacbc7bf478932a3fc4d624
Lasse Fisker <lnf@it-minds.dk>
2021-03-08 11:48:40 +0100

    feat(Map) Make map work in production

--- src/pages/kontakt.jsx
+++ src/pages/kontakt.jsx
@@ -1,51 +1,38 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import KeldgorFooter from "../components/footer";
 import KeldgorHeader from "../components/header";
 import Helmet from "react-helmet";
 import "mapbox-gl/dist/mapbox-gl.css";
-import mapboxgl from "mapbox-gl";
-
-const MAPBOX_TOKEN =
-  "pk.eyJ1Ijoic2FyYXNrIiwiYSI6ImNrZnFrZWtxMDBrMWoycXBibW42NHFrN2UifQ.YL8ZTVShTqVg16OOHKkGCA";
 
 const KontaktPage = () => {
-  // useEffect(() => {
-  //   var map = new mapboxgl.Map({
-  //     container: "map", // container id
-  //     style: "mapbox://styles/sarask/ckiocb02c4nkw17o3fzh45dpf", // style URL
-  //     zoom: 11, // starting zoo:
-  //     pitch: 35, //grader kortet vippes, gøres så kortet kan styles yderligere
-  //     bearing: -20, //Kortet drejes, gøres så kortet kan styles yderligere
-  //   });
-
-  //   var marker = new mapboxgl.Marker()
-  //     .setLngLat([9.615113, 56.188144])
-  //     .addTo(map);
-  // }, []);
-
   const mapContainerRef = useRef(null);
 
-  const [map, setMap] = useState(null);
-
   useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      accessToken: MAPBOX_TOKEN,
-      style: "mapbox://styles/sarask/ckiocb02c4nkw17o3fzh45dpf", // style URL
-      center: [9.600076, 56.182482], // starting position [lng, lat]
-      zoom: 11, // starting zoo:
-      pitch: 35, //grader kortet vippes, gøres så kortet kan styles yderligere
-      bearing: -20, //Kortet drejes, gøres så kortet kan styles yderligere
-    });
+    const script = document.createElement("script");
+    script.src = "https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js";
+    script.async = true;
+    script.onload = () => {
+      if (!window.mapboxgl) {
+        return;
+      }
+      const map = new window.mapboxgl.Map({
+        container: mapContainerRef.current,
+        accessToken:
+          "pk.eyJ1Ijoic2FyYXNrIiwiYSI6ImNrZnFrZWtxMDBrMWoycXBibW42NHFrN2UifQ.YL8ZTVShTqVg16OOHKkGCA",
+        style: "mapbox://styles/sarask/ckiocb02c4nkw17o3fzh45dpf", // style URL
+        center: [9.600076, 56.182482], // starting position [lng, lat]
+        zoom: 11, // starting zoo:
+        pitch: 35, //grader kortet vippes, gøres så kortet kan styles yderligere
+        bearing: -20, //Kortet drejes, gøres så kortet kan styles yderligere
+      });
 
-    var marker = new mapboxgl.Marker()
-      .setLngLat([9.615113, 56.188144])
-      .addTo(map);
+      var marker = new window.mapboxgl.Marker()
+        .setLngLat([9.615113, 56.188144])
+        .addTo(map);
+    };
 
-    setMap(map);
-
-    return () => map.remove();
-  }, []);
+    document.body.appendChild(script);
+  }, [mapContainerRef]);
 
   return (
     <>
@@ -53,22 +40,22 @@ const KontaktPage = () => {
         title="Keldgor | Kontakt"
         author="Sara Skovbjerg Kjeldgaard"
         description="Kontakt endelig Keldgor for at høre mere og tag et kig på kortet for hvor Keldgor er placeret."
-      />
+      ></Helmet>
       <KeldgorHeader active="Kontakt" />
       <main id="kontaktPage" className="column">
         <div id="TopTitleDescription">
           <h1>Kontakt</h1>
           <div id="divider"></div>
           <p>
-            Kontakt endelig Kelddgo for at høre mere og tag et kig på kortet for
-            hvor Kelddgo er placeret.
+            Kontakt endelig Keldgor for at høre mere og tag et kig på kortet for
+            hvor Keldgor er placeret.
           </p>
         </div>
         <div className="row" id="kontaktContent">
           <div id="kontaktText" className="column">
-            <a className="contact-footer" href="mailto:kontakt@kelddgo.dk">
+            <a className="contact-footer" href="mailto:kontakt@Keldgor.com">
               <img src="/images/Mail.svg" alt="mail" />
-              <span>kontakt@kelddgo.dk</span>
+              <span>kontakt@Keldgor.dk</span>
             </a>
             <a className="contact-footer" href="tel:004524594429">
               <img src="/images/Phone.svg" alt="phone" />
